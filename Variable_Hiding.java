5e0a026ebdc98f2b672c64aeaa666075c8256780
manahilgul003 <manahil.gul@emumba.com>
2024-07-01 15:43:53 +0500

    Create Variable_Hiding.java

---
+++ Variable_Hiding.java
@@ -0,0 +1,14 @@
+  public class Ayesha extends manahil{
+    public static int a=40;
+    public static void main(String[] args){
+        System.out.println(a);
+        System.out.println(b);
+        
+    }
+    
+}
+  public class manahil{
+      public static int a=10;
+      public static int b=20;
+      
+  }
