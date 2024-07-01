e1f96a07530ca75b6d8b744a20266e295777ba3d
manahilgul003 <manahil.gul@emumba.com>
2024-07-01 14:51:11 +0500

    Update Monster_game.java

--- Monster_game.java
+++ Monster_game.java
@@ -3,17 +3,12 @@ public class Monster_Game {
 
     public static void main(String[] args) {
 
-        // You create an object using the blueprint of this class as follows
-        // className objectName = new className();
 
         Monster Frank = new Monster();
 
-        // Since the objects name is public you can change it directly
 
         Frank.name = "Frank";
 
-        // You retrieve class field values by listing the objectName.fieldName
-        // You execute class methods by listing objectName.methodName()
 
         System.out.println(Frank.name + " has an attack value of " + Frank.getAttack());
     }
