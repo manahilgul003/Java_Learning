b60c3db832e3f4d61c4df10b1f541af55b558fed
manahilgul003 <manahil.gul@emumba.com>
2024-07-01 14:50:49 +0500

    Update Monster.java

--- Monster.java
+++ Monster.java
@@ -1,17 +1,10 @@
-// Basic class definition
-// public means this class can be used by other classes
-// Class names should begin with a capital letter
-// A file can't contain two public classes. It can contain classes that are not public
-// If you place class files in the same folder the java compiler will be able to find them
+
 
 public class Monster {
 
-    // Class Variables or Fields
-    // You declare constants with final
 
     public final String TOMBSTONE = "Here Lies a Dead monster";
 
-    // private fields are not visible outside of the class
     private int health = 500;
     private int attack = 20;
     private int movement = 2;
@@ -19,12 +12,8 @@ public class Monster {
     private int yPosition = 0;
     private boolean alive = true;
 
-    // public variables are visible outside of the class
-    // You should have as few as possible public fields
     public String name = "Big Monster";
 
-    // Class Methods
-    // Accessor Methods are used to get and set the values of private fields
 
     public int getAttack() {
         return attack;
@@ -38,10 +27,6 @@ public class Monster {
         return health;
     }
 
-    // You can create multiple versions using the same method name
-    // Now setHealth can accept an attack that contains decimals
-    // When overloading a method you can't just change the return type
-    // Focus on creating methods that accept different parameters
 
     public void setHealth(int decreaseHealth) {
         health = health - decreaseHealth;
@@ -58,28 +43,15 @@ public class Monster {
         }
     }
 
-    /* The Constructor
-     * Code that is executed when an object is created from this class definition
-     * The method name is the same as the class
-     * The constructor is only executed once per object
-     * The constructor can't return a value
-     */
 
     public Monster(int health, int attack, int movement) {
         this.health = health;
         this.attack = attack;
         this.movement = movement;
-        /* If the attributes had the same names as the class health, attack, movement
-         * You could refer to the objects fields by proceeding them with this
-         * this.health = health;
-         * this.attack = attack;
-         * objectFieldName = attributeFieldName;
-         */
+
     }
 
-    // You can overload constructors like any other method
-    // The following constructor is the one provided by default if you don't create any other constructors
-    // Default Constructor
+
 
     public Monster() {
 
@@ -93,7 +65,7 @@ public class Monster {
      *
      * public LessonSeven(int newHealth, int newAttack)
      * {
-     *      this(newHealth); // Any calls to another constructor must occur on the first line
+     *      this(newHealth); 
      *      attack = newAttack;
      * }
      */
