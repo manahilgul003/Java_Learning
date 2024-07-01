0c92f3f7b98d4d768e17a9e15fa56195806cc639
manahilgul003 <manahil.gul@emumba.com>
2024-07-01 14:45:51 +0500

    Update Exception_handling.java

--- Exception_handling.java
+++ Exception_handling.java
@@ -1,9 +1,9 @@
-import java.util.Scanner; // Library that allows us to capture user input
-import java.util.*; // Allows me to check for InputMismatchException
-import java.io.*; // Allows for system input and output through data streams, serialization and the file system
+import java.util.Scanner; 
+import java.util.*;
+import java.io.*;
 
 public class Exception_Handling{
-    // Creates a Scanner object that monitors keyboard input
+
     static Scanner userInput = new Scanner(System.in);
      
     public static void main(String[] args) {
@@ -25,25 +25,22 @@ public class Exception_Handling{
             // Exception in thread "main" java.lang.ArithmeticException
             System.out.println(a/0);
         }
-        // If the exception ArithmeticException is thrown the following execute
+       
         catch (ArithmeticException e) {
-            // Your custom error message
+
             System.out.println("You can't divide by zero");
-             
-            // Java's error message for this exception
+
             System.out.println(e.getMessage());
-             
-            // Prints the exception name and error message
+    
             System.out.println(e.toString());
-             
-            // Prints the standard error stack trace
+         
             e.printStackTrace();
         }
     }
      
     public static int checkValidAge() {
         try {
-            return userInput.nextInt(); // nextInt() receives the user input
+            return userInput.nextInt();
         }
         catch (InputMismatchException e) {
             userInput.next(); // Skips the last user input and waits for the next
@@ -72,33 +69,21 @@ public class Exception_Handling{
      
     public static void getAFile(String fileName) {
         try {
-            /* If I tried to do this without providing for an exception
-             * I'd get the error Unhandled Exception Type FileNotFoundException
-             * A checked exception is an exception the compiler forces you to protect against
-             */
+        
             FileInputStream file = new FileInputStream(fileName);
         }
         catch (FileNotFoundException e) {
             System.out.println("Sorry I couldn't find that file");
         }
-        // You can catch numerous exceptions (List most specific first
+       
         catch (IOException e) { // Catches any IO Exception
             System.out.println("An unknown IO Error Occured");
-        }
-        /* To ignore an exception do this
-         * catch (ClassNotFoundException e)
-         * {}
-         */
-        /* Java 7 allows you to catch multiple exceptions at once
-         * catch (FileNotFoundException | IOException e)
-         * {}
-         */
+      
         // This will catch any exception (This should always be last)
         catch (Exception e) {
             System.out.println("I catch every exception");
         }
-        // If used finally is always executed whether there was an exception or not
-        // It is used for clean up work like closing files and database connections
+
         finally {
             System.out.println("");
         }
