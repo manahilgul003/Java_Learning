85ff7bbf958500763f4cda9619d46588e6c8fc02
manahilgul003 <manahil.gul@emumba.com>
2024-07-03 09:45:59 +0500

    Update create_thread.java

--- create_thread.java
+++ create_thread.java
@@ -1,16 +1,8 @@
-public class create_thread extends Thread
+public class example1 extends Thread
 {
 public void run()
 {
 for(int i=0; ;i++)
  {System.out.println("This code is running in a thread");}
-}
-}
-public static void main(String[] args) {
-// TODO Auto-generated method stub
-create_thread thread = new create_thread1();
- thread.start();
- for(int i=0; ; i++)
-System.out.println("This code is outside of the thread");
 }
 }
