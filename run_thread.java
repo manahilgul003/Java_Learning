69937bac45012c1b94081afac3fe322bab5e97b7
manahilgul003 <manahil.gul@emumba.com>
2024-07-03 10:32:47 +0500

    Create run_thread.java

---
+++ run_thread.java
@@ -0,0 +1,18 @@
+public class run_thread extends Thread
+{
+public void run()
+{
+for(int i=0; ;i++)
+ {System.out.println("This code is running in a thread");}
+}
+}
+
+public class run_thread extends Thread
+{
+public void run()
+{
+for(int i=0; ;i++)
+ {System.out.println("This code is running in a thread");}
+}
+}
+
